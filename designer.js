7021376701deed148fe1e7d82168024f854ce3f4
SebCourvoisier4D <sebastien.courvoisier@4d.com>
2015-03-20 02:46:21 +0100

    First commit

---
+++ designer.js
@@ -0,0 +1,57 @@
+(function(Waktest) {
+	//    /* Default width and height of your widget */
+	Waktest.setWidth('600');
+	Waktest.setHeight('400');
+	//    /* Define custom event for your widget */
+	//    Waktest.addEvent('myEvent');
+	/* Customize existing properties */
+	Waktest.customizeProperty('runner', {
+		title: 'Runner library',
+		description: 'Runner library to use (currently supported: Mocha).'
+	});
+
+	Waktest.customizeProperty('assertion', {
+		title: 'Assertion library',
+		description: 'Assertion library to use (currently supported: Chai).'
+	});
+
+	Waktest.customizeProperty('assertionStyle', {
+		title: 'Assertion style',
+		description: 'Assertion style to use (currently supported: BDD, TDD).'
+	});
+
+	Waktest.customizeProperty('file', {
+		title: 'Test path',
+		description: 'Path to the test file or folder to run (JS script).'
+	});
+
+	Waktest.customizeProperty('automatic', {
+		title: 'Run automatically',
+		description: 'Run the attached test automatically on page load.'
+	});
+
+	Waktest.customizeProperty('runInStudio', {
+		title: 'Run in Studio',
+		description: 'Run the attached test in the Studio.'
+	});
+	//    /* Add a Label property */
+	//    Waktest.addLabel({
+	//        'defaultValue': '',
+	//        'position': 'top'
+	//    });
+	//    /* Set the Design and Styles panels */
+	//    Waktest.setPanelStyle({
+	//        'fClass': true,
+	//        'text': true,
+	//        'background': true,
+	//        'border': true,
+	//        'sizePosition': true,
+	//        'label': true,
+	//        'disabled': ['border-radius']
+	//    });
+	//    /* Override widget's initialization */
+	//    Waktest.prototype.init = function() {
+	//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
+	//    }
+});
+// For more information, refer to http://doc.wakanda.org/Wakanda/help/Title/en/page3870.html
---
+++ widget.js
@@ -0,0 +1,123 @@
+WAF.define('Waktest', ['waf-core/widget'], function(widget) {
+	var Waktest = widget.create('Waktest', {
+		init: function() {
+			this.node.innerHTML += '<div id="waktest-waf" class="waf-studio-donotsave"><span id="waktest-waf-file"></span><div id="waktest-waf-log"></div><span id="waktest-waf-label"></span><button id="waktest-waf-run" type="button" onclick="_waktestRun()">Run</button></div>';
+		},
+		runner: widget.property({
+			type: "enum",
+			"values": {
+				mocha: "Mocha"
+			},
+			bindable: false,
+			description: 'Runner library'
+		}),
+		assertion: widget.property({
+			type: "enum",
+			"values": {
+				chai: "Chai"
+			},
+			bindable: false,
+			description: 'Assertion library'
+		}),
+		assertionStyle: widget.property({
+			type: "enum",
+			"values": {
+				bdd: "BDD",
+				tdd: "TDD"
+			},
+			bindable: false,
+			description: 'Assertion style'
+		}),
+		file: widget.property({
+			type: 'string', //'file',
+			// folder: './tests',
+			// accept: '*',
+			description: 'Path to the test file or folder to run'
+		}),
+		automatic: widget.property({
+			type: 'boolean',
+			defaultValue: false,
+			bindable: false,
+			description: 'Run automatically'
+		}),
+		runInStudio: widget.property({
+			type: 'boolean',
+			defaultValue: false,
+			bindable: false,
+			description: 'Run in Studio'
+		})
+	});
+	Waktest.doAfter("init", function() {
+		var _this = this;
+		var automatic = this.automatic();
+		if ($(this.node).css('display') === 'none') {
+			// hidden ???
+		}
+		var testFilePath = null;
+		if (this.file()) {
+			testFilePath = this.file().replace(/^\/?\.\//, '');
+		}
+		if (typeof designer === 'undefined' && typeof studio === 'undefined') {
+			var match,
+				pl = /\+/g,
+				search = /([^&=]+)=?([^&]*)/g,
+				decode = function(s) {
+					return decodeURIComponent(s.replace(pl, " "));
+				},
+				query = window.location.search.substring(1),
+				urlParams = {};
+			while (match = search.exec(query)) urlParams[decode(match[1])] = decode(match[2]);
+			if (typeof urlParams['waktest-path'] !== 'undefined') {
+				testFilePath = urlParams['waktest-path'].replace(/^\/?\.\//, '');
+				automatic = true;
+			}
+		}
+		window._waktest_waf_ready = function() {
+			if (automatic === true && (typeof designer === 'undefined' || (typeof designer === 'object' && _this.runInStudio() === true)) && typeof _waktestRun === 'function') {
+				_waktestRun();
+			}
+		};
+		if (testFilePath !== null) {
+			var testLink = testFilePath; //'<a href="/' + testFilePath + '" target="_blank">' + testFilePath + '</a>';
+			var libraryURL = '/waktest-waf-lib?runner=' + this.runner() + '&assertion=' + this.assertion() + '&assertionStyle=' + this.assertionStyle() + '&path=' + testFilePath + '&widgetId=' + this.id;
+			if (typeof urlParams !== 'undefined' && typeof urlParams['waktest-format'] !== 'undefined') {
+				libraryURL += '&format=' + urlParams['waktest-format'];
+			}
+			var cssURL = '/waktest-waf-css?runner=' + this.runner() + '&widgetId=' + this.id;
+			if (typeof designer === 'object' && typeof studio === 'object') {
+				// @TODO: check server availability
+				try {
+					var serverAddress = studio.getRemoteServerInfo().split(':');
+					var serverPort = /\sport="(\d+)"/i.exec(studio.File(studio.editor.getProjectPath() + 'Settings.waSettings').toString())[1];
+					libraryURL = serverAddress[0] + ':' + serverAddress[1] + ':' + serverPort + libraryURL;
+					cssURL = serverAddress[0] + ':' + serverAddress[1] + ':' + serverPort + cssURL;
+				} catch (e) {
+					libraryURL = 'http://127.0.0.1:8081' + libraryURL;
+					cssURL = 'http://127.0.0.1:8081' + cssURL;
+				}
+			}
+			if (typeof designer === 'undefined' || (typeof designer === 'object' && this.runInStudio() === true)) {
+				var waktestScript = document.createElement('script');
+				waktestScript.type = 'application/javascript';
+				waktestScript.src = libraryURL;
+				var waktestLink = document.createElement('link');
+				waktestLink.rel = 'stylesheet';
+				waktestLink.type = 'text/css';
+				waktestLink.href = cssURL;
+				document.getElementsByTagName('head')[0].appendChild(waktestScript);
+				document.getElementsByTagName('head')[0].appendChild(waktestLink);
+			}
+			if (automatic === true) {
+				$('#waktest-waf-file', this.node).html(testLink + ' (auto)');
+				$('#waktest-waf-run', this.node).prop('disabled', true);
+			} else {
+				$('#waktest-waf-file', this.node).html(testLink);
+				$('#waktest-waf-run', this.node).prop('disabled', false);
+			}
+		} else {
+			$('#waktest-waf-file', this.node).html('No test specified');
+			$('#waktest-waf-run', this.node).prop('disabled', true);
+		}
+	});
+	return Waktest;
+});
