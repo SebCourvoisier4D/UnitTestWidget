24adfb2c87b8ba33147f2ee4c57817f07958878b
SebCourvoisier <sebastien.courvoisier@4d.com>
2015-04-10 16:00:35 +0200

    Rename the Widget

--- designer.js
+++ designer.js
@@ -1,46 +1,46 @@
-(function(UnitTest) {
+(function(UnitTestWidget) {
 	//    /* Default width and height of your widget */
-	UnitTest.setWidth('250');
-	UnitTest.setHeight('48');
+	UnitTestWidget.setWidth('250');
+	UnitTestWidget.setHeight('48');
 	//    /* Define custom event for your widget */
-	//    UnitTest.addEvent('myEvent');
+	//    UnitTestWidget.addEvent('myEvent');
 	/* Customize existing properties */
-	/*UnitTest.customizeProperty('runner', {
+	/*UnitTestWidget.customizeProperty('runner', {
 		title: 'Runner library',
 		description: 'Runner library to use (currently supported: Mocha).'
 	});
 
-	UnitTest.customizeProperty('assertion', {
+	UnitTestWidget.customizeProperty('assertion', {
 		title: 'Assertion library',
 		description: 'Assertion library to use (currently supported: Chai).'
 	});
 
-	UnitTest.customizeProperty('assertionStyle', {
+	UnitTestWidget.customizeProperty('assertionStyle', {
 		title: 'Assertion style',
 		description: 'Assertion style to use (currently supported: BDD, TDD).'
 	});*/
 
-	UnitTest.customizeProperty('file', {
+	UnitTestWidget.customizeProperty('file', {
 		title: 'Test path',
 		description: 'Path to the test file or folder to run (JS script).'
 	});
 
-	UnitTest.customizeProperty('automatic', {
+	UnitTestWidget.customizeProperty('automatic', {
 		title: 'Run automatically',
 		description: 'Run the attached test automatically on page load.'
 	});
 
-	UnitTest.customizeProperty('runInStudio', {
+	UnitTestWidget.customizeProperty('runInStudio', {
 		title: 'Run in Studio',
 		description: 'Run the attached test in the Studio.'
 	});
 	//    /* Add a Label property */
-	//    UnitTest.addLabel({
+	//    UnitTestWidget.addLabel({
 	//        'defaultValue': '',
 	//        'position': 'top'
 	//    });
 	//    /* Set the Design and Styles panels */
-	//    UnitTest.setPanelStyle({
+	//    UnitTestWidget.setPanelStyle({
 	//        'fClass': true,
 	//        'text': true,
 	//        'background': true,
@@ -50,7 +50,7 @@
 	//        'disabled': ['border-radius']
 	//    });
 	//    /* Override widget's initialization */
-	//    UnitTest.prototype.init = function() {
+	//    UnitTestWidget.prototype.init = function() {
 	//        this.node.innerHTML = "Widget Text"; /* Include text inside the widget */
 	//    }
 });
